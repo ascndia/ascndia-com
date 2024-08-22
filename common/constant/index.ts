24a6e75e1840b83c9f18144ce7b1e111f5cade18
ascndia <fixgmc@gmail.com>
2024-08-22 13:30:03 +0700

    separated assets and content

--- common/constant/index.ts
+++ common/constant/index.ts
@@ -3,6 +3,6 @@ export const PLACEHOLDER_URL =
 export const IG_PROFILE = 'https://www.instagram.com/ascndia/';
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/';
 export const PAGESPEED_CATEGORIES = '&category=accessibility&category=performance&category=best-practices&category=seo';
-export const PROFILE_URL ='/pp.jpeg';
+export const PROFILE_URL = 'https://raw.githubusercontent.com/ascndia/data/master/misc/pp.jpeg';
 export const SAWERIA_URL = 'https://saweria.co/ascndia';
 export const DEVTO_BLOG_API = 'https://dev.to/api/articles?username=codebayu';
--- fetch/data.ts
+++ fetch/data.ts
@@ -1,12 +1,12 @@
 import { IProject, IService, ISkill } from '@/services/data/types';
 
 export const getSkills = (): Promise<ISkill[]> => {
-  return fetch('/data/skills.json').then(res => res.json());
+  return fetch('https://raw.githubusercontent.com/ascndia/data/master/json/skills.json').then(res => res.json());
 };
 export const getServices = (): Promise<IService[]> => {
-  return fetch('/data/services.json').then(res => res.json());
+  return fetch('https://raw.githubusercontent.com/ascndia/data/master/json/services.json').then(res => res.json());
 };
 
 export const getProjects = (): Promise<IProject[]> => {
-  return fetch('/data/projects.json').then(res => res.json());
+  return fetch('https://raw.githubusercontent.com/ascndia/data/master/json/projects.json').then(res => res.json());
 };
